bb80d2d8eef9c7b6ec1b2de0358c8e96662ad0cc
unknown <gopisettysaikarthik9@gmail.com>
2023-10-02 10:12:10 +0530

    Subscription Updated

---
+++ ReviewService/Model/SubscriptionContext.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Driver;
+
+namespace SubscriptionService.Model
+{
+    public class SubscriptionContext
+    {
+        MongoClient client;
+        IMongoDatabase database;
+        public SubscriptionContext(IConfiguration config)
+        {
+            client = new MongoClient(config.GetConnectionString("MyMongodbCon"));
+            database = client.GetDatabase(config.GetSection("DatabaseName").Value);
+        }
+        public IMongoCollection<Subscription> subscriptions => database.GetCollection<Subscription>("Subscriptions");
+    }
+}
