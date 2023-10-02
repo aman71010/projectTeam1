b34d04ac1bba000c25e4f19e18123d774a6c0517
Aman <amanraj2261@gmail.com>
2023-10-02 12:55:22 +0530

    api gateway first save

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,20 +1,13 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+builder.Configuration.AddJsonFile("Ocelot.json");
+builder.Services.AddOcelot();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseOcelot();
 
 app.UseHttpsRedirection();
 
