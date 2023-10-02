0c6b573d7d83a69c6fbfd4d7da5aa89e4d7b150f
mehul parmar <parmarmehul2310@gmail.com>
2023-10-02 19:20:22 +0530

    added all required orderservice

---
+++ ReservationService/Excecption/OrderDoesNotExistExcecption.cs
@@ -0,0 +1,15 @@
+﻿namespace OrderService.Excecption
+{
+    public class OrderDoesNotExistExcecption : Exception
+    {
+        public OrderDoesNotExistExcecption()
+        {
+        }
+        public OrderDoesNotExistExcecption(string message):base(message)
+        { 
+        
+        }
+        
+
+    }
+}
---
+++ ReservationService/Services/OrderServices.cs
@@ -0,0 +1,87 @@
+﻿using OrderService.Excecption;
+using OrderService.Models;
+using OrderService.Repository;
+
+namespace OrderService.Services
+{
+    public class OrderServices : IOrderService
+    {
+        private readonly IOrderRepository Repo;
+        public OrderServices(IOrderRepository Repo)
+        {
+            this.Repo = Repo;
+        }
+        public void CancelOrder(string id)
+        {
+           var res =Repo.GetOrder(id);
+            if (res != null) 
+            {
+                Repo.CancelOrder(id);
+            }
+            else
+            {
+                throw new OrderDoesNotExistExcecption($"order with id {id} does not exists");
+            }
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            return Repo.GetAllOrders();
+        }
+
+        public Order GetOrder(string id)
+        {
+            var res = Repo.GetOrder(id);
+            if (res != null)
+            {
+                return Repo.GetOrder(id);
+            }
+            else
+            {
+                throw new OrderDoesNotExistExcecption($"order with id {id} does not exists");
+
+            }
+        }
+
+        public Order GetOrderByEmail(string email)
+        {
+            var res = Repo.GetOrderByEmail(email);
+            if (res != null)
+            {
+                return Repo.GetOrderByEmail(email);
+            }
+            else
+            {
+                throw new OrderDoesNotExistExcecption($"{email} does not exists");
+
+            }
+        }
+
+        public void PlaceOrder(Order order)
+        {
+            var res = Repo.GetOrder(order.Order_Id);
+            if (res != null)
+            {
+                throw new OrderAlreadyExistExcecption($"order with id {order.Order_Id} already exists");
+            }
+            else
+            {
+                Repo.PlaceOrder(order);
+            }
+        }
+
+        public void UpdateOrder(string id, Order order)
+        {
+            var res = Repo.GetOrder(id);
+            if (res != null)
+            {
+                Repo.UpdateOrder(id, order);
+            }
+            else
+            {
+                throw new OrderDoesNotExistExcecption($"order with id {id} does not exists");
+
+            }
+        }
+    }
+}
