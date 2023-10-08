f20d81c8f1a9d9f37cd70cfaf1b44ccaa0b2062b
dineshwar <dineshwar.prayas22jee19@gmail.com>
2023-10-08 05:24:13 +0000

    created service for api call in angular

--- angular/webapp/src/app/app-routing.module.ts
+++ angular/webapp/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { OrderComponent } from './order/order.component'; 
-
 const routes: Routes = [
   {path:"login",component:LoginComponent},
 {path:"register",component:RegisterComponent},
