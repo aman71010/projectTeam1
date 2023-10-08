68a63df136df2341004e730601d3d77e5ce9d09a
bilwaraj333 <bilwaraj333@gmail.com>
2023-10-08 16:14:07 +0000

    added menu Description

--- angular/webapp/src/app/app-routing.module.ts
+++ angular/webapp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuDescriptionComponent } from './menu-description/menu-description.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'menudiscriprion',component:MenuDescriptionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/webapp/src/app/app.module.ts
+++ angular/webapp/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuDescriptionComponent } from './menu-description/menu-description.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule} from '@angular/material/card'
+import { MatDividerModule } from '@angular/material/divider'; // Import MatDividerModule
+import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuDescriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatCardModule,MatDividerModule, // Add MatDividerModule to your imports
+    MatIconModule,CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
