6928b3a21fcd49ecd3358a1b03c78d741fe0dcd7
unknown <gopisettysaikarthik9@gmail.com>
2023-10-09 12:14:34 +0530

    Decrease The Height in Subscription UI

--- angular/webapp/src/app/app-routing.module.ts
+++ angular/webapp/src/app/app-routing.module.ts
@@ -3,14 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
-<<<<<<< HEAD
-import { OrderComponent } from './order/order.component'; 
+import { ProfileComponent } from './profile/profile.component'; 
 import { SubscriptionComponent } from './subscription/subscription.component';
-=======
 import { MenuComponent } from './menu/menu.component';
 import { OrderComponent } from './order/order.component';
->>>>>>> 81c3fb1cc5e9896d5d51cbc7b2306a26b4cdd143
 
 const routes: Routes = [
   {path: 'checkout',component: CheckoutComponent},
