45026c3057a05a942c4c3f1265f17d86c4e48634
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-10-11 03:18:36 +0530

    updated, added required changes

---
+++ CarInventoryService/DataModel/MenuData.cs
@@ -0,0 +1,15 @@
+﻿namespace MenuService.DataModel
+{
+    public class MenuData
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+        public double Price {get; set; }
+
+        public IFormFile Image { get; set; }
+
+    }
+}
