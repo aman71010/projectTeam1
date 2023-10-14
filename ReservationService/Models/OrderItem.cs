6fe3892daa649a4500d3dd6ef0018e4dea232e40
mehul parmar <parmarmehul2310@gmail.com>
2023-10-14 11:00:34 +0530

    order history

--- ReservationService/Models/OrderItem.cs
+++ ReservationService/Models/OrderItem.cs
@@ -3,7 +3,8 @@
     
         public class OrderItem
         {  // public int id { get; set; }
-            public int MenuItemId { get; set; }
+
+            public string MenuItemId { get; set; }
             public int Quantity { get; set; }
             public string Name { get; set; }
         }
--- angular/webapp/src/app/order/order.component.ts
+++ angular/webapp/src/app/order/order.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MenuItem } from '../Models/MenuItem';
 import { Order } from '../Models/Order';
+import { OrderItem } from '../Models/OrderItem';
 import { OrderService } from 'src/OrderService/order.service';
 import { ActivatedRoute, Router } from '@angular/router';
-<<<<<<< HEAD
 import { MenuService } from '../Services/MenuService/menu.service';
 import { Observable, map } from 'rxjs';
+import { DomSanitizer ,SafeUrl} from '@angular/platform-browser';
 
-=======
 import { PaginatePipe } from 'ngx-pagination';
->>>>>>> 32a721ea4d9ffe607b233e8f15fb4a58f56f2b84
 
 @Component({
   selector: 'app-order',
@@ -19,27 +18,39 @@ import { PaginatePipe } from 'ngx-pagination';
 export class OrderComponent implements OnInit {
   
   
-  order: any = ""
-  orderimage: any = ""
-  orderobj: any = {
-  orderimg: this.orderimage, orderdata: this.order
-  }
-  orders: any = [];
+  // order: any = ""
+  // orderimage: any = ""
+  // orderobj: any = {
+  // orderimg: this.orderimage, orderdata: this.order
+  // }
+  orders: any ;
+  menuitem:any;
   
   p: number = 1;
   //orders :any;
-
-  constructor(private svc: OrderService, private menuservice: MenuService) { }
+ 
+  constructor(private svc: OrderService, private menuservice: MenuService ,private sanitizer: DomSanitizer,) { }
+  getImage(imageData: any)
+  {                                           
+    const imageUrl = 'data:image/jpeg;base64,' + imageData;
+    return this.sanitizer.bypassSecurityTrustUrl(imageUrl);
+  }
 
   ngOnInit() {
     
    
-   /* this.svc.getOrdersByEmail("mehul@example.com").subscribe((data :any)=>
+   this.svc.getOrdersByEmail("user@example.com").subscribe((data :any)=>
     {
+      console.log(data);
       this.orders=data;
-    }
+    })
     
-    )*/
+    this.menuservice.getMenuItemById("65297c0cf370f2a89e9b2bd8").subscribe((data1 :any)=>
+    {
+      console.log(data1);
+      this.menuitem=data1;
+    }
+    )
 
 
     /*let o :any 
@@ -62,22 +73,22 @@ export class OrderComponent implements OnInit {
 
 
     
-    /*this.svc.getOrdersByEmail("mehul@example.com").subscribe((data: any) => {
-      this.orders = data.map((order: any) => {
-        return this.menuservice.getMenuItemById(order.items[0].menuItemId).pipe(
-          map((results: any) => {
-            const orderData = order;
-            const orderImg = results.image;
-            console.log('Order Data:', orderData);
-            console.log('Order Image:', orderImg);
-            return {
-              orderData,
-              orderImg
-            };
-          })
-        );
-      });
-    });*/
+    // this.svc.getOrdersByEmail("use@example.com").subscribe((data: any) => {
+    //   this.orders = data.map((order: any) => {
+    //     return this.menuservice.getMenuItemById(order.items[0].menuItemId).pipe(
+    //       map((results: any) => {
+    //         const orderData = order;
+    //         const orderImg = results.image;
+    //         console.log('Order Data:', orderData);
+    //         console.log('Order Image:', orderImg);
+    //         return {
+    //           orderData,
+    //           orderImg
+    //         };
+    //       })
+    //     );
+    //   });
+    // });
   }
 
 }
