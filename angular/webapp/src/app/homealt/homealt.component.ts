cf12ad2bd5af39a30bc05f6dae3e79441c3e38dc
dineshwar <dineshwar.prayas22jee19@gmail.com>
2023-10-16 03:38:21 +0000

    worked on forgot and registration componant and also created a alternate home componant

---
+++ angular/webapp/src/app/homealt/homealt.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homealt',
+  templateUrl: './homealt.component.html',
+  styleUrls: ['./homealt.component.css']
+})
+export class HomealtComponent {
+
+}
